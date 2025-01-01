79dbfb909508dae0111ae341d716d25205c3da0a
SimeonKovachev <skcodingacc@gmail.com>
2025-01-01 18:44:41 +0200

    Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,4 @@
-"use client";
-
-import PreLoader from "@/components/Common/PreLoader";
-import "../styles/globals.css";
-import { useEffect, useState } from "react";
-import Footer from "@/components/Footer";
-import ScrollToTop from "@/components/ScrollToTop";
-import ToasterContext from "./api/context/ToasterContext";
-import { Montserrat } from "next/font/google";
-import { usePathname } from "next/navigation";
-import Navbar from "@/components/Navbar";
-import Head from "next/head";
-import { GoogleAnalytics } from "@next/third-parties/google";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-subheading",
-});
+import RootLayout from "@/components/RootLayout";
 
 export const metadata = {
   title: {
@@ -119,80 +101,15 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({
+export default function SiteLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState(true);
-  const pathname = usePathname();
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 2000);
-  }, []);
-
-  const isStudio = pathname.startsWith("/studio");
-
   return (
-    <html
-      suppressHydrationWarning={true}
-      lang="bg"
-      className={`${montserrat.variable}`}
-    >
-      <GoogleAnalytics gaId="G-JFDSDGDFG" />
-      <head>
-        <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
-        <link rel="robots" href="/robots.txt" />
-        <link rel="icon" href="/images/favicon.ico" />
-      </head>
+    <html lang="bg">
       <body>
-        {loading ? (
-          <PreLoader />
-        ) : (
-          <>
-            {!isStudio && <Navbar />}
-            <ToasterContext />
-            <main>{children}</main>
-            {!isStudio && <Footer />}
-            {!isStudio && <ScrollToTop />}
-          </>
-        )}
-
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "TattooParlor",
-              name: "Ink Spell Tattoo Studio",
-              image:
-                "https://www.ink-spell.com/images/ink-spell-full-colored-logo.jpg",
-              description:
-                "Professional tattoo studio in Pleven, Bulgaria specializing in unique, artistic tattoos, including astrology and tarot-inspired designs.",
-              address: {
-                "@type": "PostalAddress",
-                streetAddress: "ул. Васил Априлов 48",
-                addressLocality: "Плевен",
-                addressRegion: "Плевен",
-                postalCode: "5800",
-                addressCountry: "BG",
-              },
-              geo: {
-                "@type": "GeoCoordinates",
-                latitude: "43.4168", // Replace with actual latitude
-                longitude: "24.6062", // Replace with actual longitude
-              },
-              url: "https://www.ink-spell.com",
-              telephone: "+359894300545",
-              openingHours: "Mo-Sa 10:00-19:00",
-              priceRange: "$$",
-              sameAs: [
-                "https://www.facebook.com/StudioInkSpell",
-                "https://www.instagram.com/inkspell.tattoo",
-              ],
-            }),
-          }}
-        />
+        <RootLayout>{children}</RootLayout>
       </body>
     </html>
   );
---
+++ src/components/RootLayout.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import PreLoader from "@/components/Common/PreLoader";
+import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import Footer from "@/components/Footer";
+import ScrollToTop from "@/components/ScrollToTop";
+import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
+import Navbar from "@/components/Navbar";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import ToasterContext from "@/app/api/context/ToasterContext";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-subheading",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [loading, setLoading] = useState(true);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
+
+  const isStudio = pathname.startsWith("/studio");
+
+  return (
+    <html
+      suppressHydrationWarning={true}
+      lang="bg"
+      className={`${montserrat.variable}`}
+    >
+      <GoogleAnalytics gaId="G-JFDSDGDFG" />
+      <head>
+        <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
+        <link rel="robots" href="/robots.txt" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </head>
+      <body>
+        {loading ? (
+          <PreLoader />
+        ) : (
+          <>
+            {!isStudio && <Navbar />}
+            <ToasterContext />
+            <main>{children}</main>
+            {!isStudio && <Footer />}
+            {!isStudio && <ScrollToTop />}
+          </>
+        )}
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "TattooParlor",
+              name: "Ink Spell Tattoo Studio",
+              image:
+                "https://www.ink-spell.com/images/ink-spell-full-colored-logo.jpg",
+              description:
+                "Professional tattoo studio in Pleven, Bulgaria specializing in unique, artistic tattoos, including astrology and tarot-inspired designs.",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "ул. Васил Априлов 48",
+                addressLocality: "Плевен",
+                addressRegion: "Плевен",
+                postalCode: "5800",
+                addressCountry: "BG",
+              },
+              geo: {
+                "@type": "GeoCoordinates",
+                latitude: "43.4168", // Replace with actual latitude
+                longitude: "24.6062", // Replace with actual longitude
+              },
+              url: "https://www.ink-spell.com",
+              telephone: "+359894300545",
+              openingHours: "Mo-Sa 10:00-19:00",
+              priceRange: "$$",
+              sameAs: [
+                "https://www.facebook.com/StudioInkSpell",
+                "https://www.instagram.com/inkspell.tattoo",
+              ],
+            }),
+          }}
+        />
+      </body>
+    </html>
+  );
+}
